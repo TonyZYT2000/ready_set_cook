1a89cebd3132f340ae0e3c2ffa47b2423f83a1e5
Yutong Zhang <zhangyt2000@gmail.com>
2020-11-05 20:51:41 -0800

    Setup nav bar

---
+++ lib/signup.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(child: Text("Sign Up", style: TextStyle(fontSize: 18)));
+  }
+}
