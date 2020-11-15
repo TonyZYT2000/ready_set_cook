0ee8fd71c1b2c7bdba3ea974852daead6353a986
rickyli888 <rickyli860@gmail.com>
2020-11-15 18:10:29 -0500

    Push Custom Grocery Item component

---
+++ lib/models/groceryItem.dart
@@ -0,0 +1,8 @@
+class Item {
+  final String productName;
+  final int quantity;
+  final int expirationTime;
+  final String imageID;
+
+  Item(this.productName, this.quantity, this.expirationTime,this.imageID);
+}
