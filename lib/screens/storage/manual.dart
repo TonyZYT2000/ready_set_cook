b58109f53027ec99ece39b9eb064d14d227f76f2
rickyli888 <rickyli860@gmail.com>
2020-11-15 23:29:13 -0500

    Incomplete Manual Input Entry for Storage

--- lib/screens/storage/manual.dart
+++ lib/screens/storage/manual.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ready_set_cook/shared/constants.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class Manual extends StatefulWidget {
   @override
@@ -6,8 +12,71 @@ class Manual extends StatefulWidget {
 }
 
 class _ManualState extends State<Manual> {
+
+  // Cloud Database Reference
+  final cloudReference = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Manual Page', style: TextStyle(fontSize: 12)));
+
+    // text field state
+    String _productName = '';
+    int _quantity;
+    int _expirationTime;
+
+    return Scaffold(
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            TextFormField(
+              decoration:
+              textInputDecoration.copyWith(hintText: 'Item Name'),
+              onChanged: (val) {
+                setState(() => _productName = val);
+              },
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.number,
+              decoration:
+              textInputDecoration.copyWith(hintText: 'Quantity'),
+              validator: (val) => val.isEmpty ? 'Enter Quantity' : null,
+              onChanged: (val) {
+                setState(() => _quantity = int.parse(val));
+              },
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.number,
+              decoration: textInputDecoration.copyWith(hintText: 'Expiration Time'),
+              onChanged: (val) {
+                setState(() => _expirationTime = int.parse(val));
+              },
+            ),
+
+            SizedBox(height: 20.0),
+            RaisedButton(
+                color: Colors.blue[400],
+                child: Text(
+                  'Submit',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                    cloudReference.collection("items").add(
+                        {
+                          "productName" : _productName,
+                          "quantity" : _quantity,
+                          "expirationTime" : _expirationTime,
+                        }).then((value){
+                      print(value.id);
+                    });
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
