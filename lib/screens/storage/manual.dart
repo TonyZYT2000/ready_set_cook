5e1a1d1fc9c83e3c6cbc5e85bde736ac82f6b4f3
xzy19981017 <jimboxzy@gmail.com>
2020-11-15 22:10:43 -0800

    Resolved conflicts, but needs to implement code again, sorry.

--- lib/screens/storage/manual.dart
+++ lib/screens/storage/manual.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:ready_set_cook/shared/constants.dart';
 import 'package:ready_set_cook/models/ingredient.dart';
 import 'package:intl/intl_browser.dart';
-=======
-import 'package:flutter/services.dart';
-import 'package:ready_set_cook/shared/constants.dart';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
->>>>>>> b58109f53027ec99ece39b9eb064d14d227f76f2
 
 class Manual extends StatefulWidget {
   @override
@@ -18,7 +9,6 @@ class Manual extends StatefulWidget {
 }
 
 class _ManualState extends State<Manual> {
-<<<<<<< HEAD
   final _formKey = GlobalKey<FormState>();
   String _ingredientName = '';
   int _quantity = 0;
@@ -64,72 +54,6 @@ class _ManualState extends State<Manual> {
                 ],
               ),
             )),
-=======
-
-  // Cloud Database Reference
-  final cloudReference = FirebaseFirestore.instance;
-
-  @override
-  Widget build(BuildContext context) {
-
-    // text field state
-    String _productName = '';
-    int _quantity;
-    int _expirationTime;
-
-    return Scaffold(
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            SizedBox(height: 20.0),
-            TextFormField(
-              decoration:
-              textInputDecoration.copyWith(hintText: 'Item Name'),
-              onChanged: (val) {
-                setState(() => _productName = val);
-              },
-            ),
-            SizedBox(height: 20.0),
-            TextFormField(
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              keyboardType: TextInputType.number,
-              decoration:
-              textInputDecoration.copyWith(hintText: 'Quantity'),
-              validator: (val) => val.isEmpty ? 'Enter Quantity' : null,
-              onChanged: (val) {
-                setState(() => _quantity = int.parse(val));
-              },
-            ),
-            SizedBox(height: 20.0),
-            TextFormField(
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              keyboardType: TextInputType.number,
-              decoration: textInputDecoration.copyWith(hintText: 'Expiration Time'),
-              onChanged: (val) {
-                setState(() => _expirationTime = int.parse(val));
-              },
-            ),
-
-            SizedBox(height: 20.0),
-            RaisedButton(
-                color: Colors.blue[400],
-                child: Text(
-                  'Submit',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                    cloudReference.collection("items").add(
-                        {
-                          "productName" : _productName,
-                          "quantity" : _quantity,
-                          "expirationTime" : _expirationTime,
-                        }).then((value){
-                      print(value.id);
-                    });
-                }),
-          ],
-        ),
->>>>>>> b58109f53027ec99ece39b9eb064d14d227f76f2
       ),
     );
   }
