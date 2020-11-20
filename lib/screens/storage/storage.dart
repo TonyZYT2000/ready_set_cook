5c30fb748bc0d7bf605dc4a5870c7363f4cff11a
xzy19981017 <jimboxzy@gmail.com>
2020-11-19 20:00:48 -0800

    Having issues with display storage

--- lib/screens/storage/storage.dart
+++ lib/screens/storage/storage.dart
@@ -1,6 +1,5 @@
 import 'package:ready_set_cook/screens/storage/widget/storage_row.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:ready_set_cook/screens/storage/add.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
