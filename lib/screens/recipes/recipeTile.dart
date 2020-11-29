ca8c916194a229731aa72e8641254d444a16524a
h4wang489 <63083869+h4wang489@users.noreply.github.com>
2020-11-28 18:44:43 -0800

    Update recipeTile.dart

--- lib/screens/recipes/recipeTile.dart
+++ lib/screens/recipes/recipeTile.dart
@@ -1,31 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:ready_set_cook/models/recipe.dart';
+import 'package:ready_set_cook/screens/recipes/view_recipe.dart';
 
 class RecipeTile extends StatelessWidget {
-
   final String name;
-
-  RecipeTile( { this.name } );
-
+  final bool cookedBefore;
+  final int rating;
+  final int recipeId;
+  List<String> lst = ['Name: ', 'Cooked before: ', 'Rating:  '];
+  RecipeTile({this.name, this.cookedBefore, this.rating, this.recipeId});
   @override
   Widget build(BuildContext context) {
-    if(name == "") {
-      return Container();
+    String cooked = "";
+    if (cookedBefore == false) {
+      cooked = "No";
+    } else {
+      cooked = "Yes";
     }
-
-    return Padding(
-      padding: EdgeInsets.only(top: 8.0),
-      child: Card(
-        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            radius: 30.0,
-            backgroundColor: Colors.blue[300],
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ViewRecipe(recipeId)));
+        },
+        child: Container(
+            child: InputDecorator(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue[100], width: 100),
+              borderRadius: BorderRadius.circular(20.0),
             ),
-            title: Text(name),
-            // subtitle: Text('Rating: ${recipe.rating}/5'),
-        ),
-      )
-    );
+          ),
+          child: Text(
+              lst[0] +
+                  name +
+                  '   ' +
+                  lst[1] +
+                  cooked +
+                  '   ' +
+                  lst[2] +
+                  rating.toString(),
+              textAlign: TextAlign.center),
+        ))
+        // Container(
+        //   padding: EdgeInsets.all(90.0),
+        //   height: 160,
+        //   width: 320,
+        //   decoration: BoxDecoration(
+        //       border: Border.all(color: Colors.blue[100], width: 10),
+        //       borderRadius: BorderRadius.circular(20),
+        //       image: DecorationImage(
+        //           colorFilter: ColorFilter.mode(
+        //               Colors.blue.withOpacity(1.0), BlendMode.softLight),
+        //           fit: BoxFit.cover,
+        //           image: AssetImage("assets/images/apple.png"))),
+        // )
+        );
   }
 }
