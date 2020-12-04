8239b8695d3d683fe3f3f60e746b56737700e144
h4wang489 <h4wang@ucsd.edu>
2020-12-04 11:17:42 -0800

    no change

--- lib/screens/recipes/createRecipe.dart
+++ lib/screens/recipes/createRecipe.dart
@@ -24,66 +24,6 @@ class _CreateRecipeState extends State<CreateRecipe> {
   List<String> _instructions = [];
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    final uid = FirebaseAuth.instance.currentUser.uid;
-    final recipeDB = RecipesDatabaseService(uid: uid);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Recipe'),
-        backgroundColor: Colors.cyan,
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                        hintText: 'Enter Ingredient Name'),
-                    onChanged: (val) {
-                      setState(() => _recipeName = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                        hintText: 'Enter Quantity'),
-                    onChanged: (val) {
-                      setState(() => _quantity = int.parse(val));
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration:
-                        textInputDecoration.copyWith(hintText: 'Enter Unit'),
-                    onChanged: (val) {
-                      setState(() => _unit = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                      color: Colors.blue[400],
-                      child: Text(
-                        'Add Recipe',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () async {
-                        recipeDB.addCustomRecipe(new Recipe(
-                            recipeId: _recipeId,
-                            name: _recipeName,
-                            ingredients: _ingredients,
-                            instructions: _instructions,
-                            rating: _rating,
-                            numRatings:
-                                _numRatings)); // adds to all recipes and personal collection
-                      }),
-                ],
-              ),
-            )),
-=======
     final _uid = FirebaseAuth.instance.currentUser.uid;
     final recipeDB = RecipesDatabaseService(uid: _uid);
     final _tabPages = <Widget>[
@@ -136,7 +76,6 @@ class _CreateRecipeState extends State<CreateRecipe> {
         body: TabBarView(
           children: _tabPages,
         ),
->>>>>>> 495cf66b27fa01ce43802ae5b5e63065c77c5d46
       ),
     );
   }
