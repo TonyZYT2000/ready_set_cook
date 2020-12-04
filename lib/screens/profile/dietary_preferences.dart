b69984f3504977539eb7bbbd689916fe899dceaa
Kaiyuan Hu <you@example.com>
2020-12-04 14:27:19 +0800

    profile dietary perf update

--- lib/screens/profile/dietary_preferences.dart
+++ lib/screens/profile/dietary_preferences.dart
@@ -6,6 +6,7 @@ import 'package:ready_set_cook/screens/profile/profile.dart';
 import 'package:ready_set_cook/screens/home/home.dart';
 import 'package:ready_set_cook/services/allergy_service.dart';
 import 'package:ready_set_cook/models/allergies.dart';
+import 'package:ready_set_cook/screens/profile/add_allergy.dart';
 
 class Allergy extends StatefulWidget {
   final Function toggleView;
@@ -20,8 +21,6 @@ class _AllergyState extends State<Allergy> {
   final _formKey = GlobalKey<FormState>();
   AllergyDatabase _allergyDB = null;
 
-  String dropdownValue = 'Vegan';
-
   @override
   Widget build(BuildContext context) {
     if (_allergyDB == null) {
@@ -29,82 +28,55 @@ class _AllergyState extends State<Allergy> {
     }
 
     return Scaffold(
-      backgroundColor: Colors.blue[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[120],
-        elevation: 0.0,
-        title: Text('Dietary Preference'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 40.0),
-                  Text(
-                    "Add your dietary preference",
-                    style: new TextStyle(
-                      fontSize: 30.0,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 40.0),
-                  DropdownButton<String>(
-                    value: dropdownValue,
-                    icon: Icon(Icons.arrow_downward),
-                    iconSize: 30,
-                    elevation: 16,
-                    style: TextStyle(color: Colors.deepPurple),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
-                    ),
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue = newValue;
+        backgroundColor: Colors.blue[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[120],
+          elevation: 0.0,
+          title: Text('Dietary Preference'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Add_allergy()),
+            );
+          },
+          child: Icon(Icons.add),
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0))),
+            child: StreamBuilder(
+                stream: _allergyDB.getAllergySnap(),
+                builder: (ctx, allergySnapshot) {
+                  if (allergySnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  final allergySnap = allergySnapshot.data.documents;
+                  return ListView.builder(
+                      itemCount: allergySnap.length,
+                      itemBuilder: (ctx, index) {
+                        final String allergy_id = allergySnap[index].id;
+
+                        return Dismissible(
+                          onDismissed: (DismissDirection direction) {
+                            setState(() {
+                              _allergyDB.deleteAllergy(allergy_id);
+                            });
+                            showDialog(
+                                context: context,
+                                builder: (_) => AlertDialog(
+                                      title: Text("Allergy deleted"),
+                                    ));
+                          },
+                          background: Container(color: Colors.red),
+                          child: ListTile(title: Text('$allergy_id')),
+                          key: Key(allergy_id),
+                        );
                       });
-                    },
-                    items: <String>[
-                      'Vegan',
-                      'Peanut Allergy',
-                      'Lactose Intolerant',
-                      'Seafood Allergy'
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 40.0),
-                  RaisedButton(
-                      color: Colors.blue[400],
-                      child: Text(
-                        'Add',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      //onPressed: () async {}
-
-                      onPressed: () async {
-                        _allergyDB.addAllergy(DiePref(dropdownValue));
-
-                        //Navigator.of(context).pop();
-                        showDialog(
-                            context: context,
-                            builder: (_) => AlertDialog(
-                                  title: Text('Added ' + dropdownValue + '!'),
-                                  content: Text('You can add more or delete'),
-                                ));
-                      }),
-
-                  SizedBox(height: 12.0),
-                  //BigLogo()
-                ],
-              ),
-            )),
-      ),
-    );
+                })));
   }
 }
