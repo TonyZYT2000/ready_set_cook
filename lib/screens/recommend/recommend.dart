b2cfb77bbb1adb1776e14f4c8e069882b21f293c
rickyli888 <rickyli860@gmail.com>
2020-12-05 17:11:01 -0500

    Start

---
+++ lib/screens/recommend/recommend.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
+class Recommend extends StatefulWidget {
+  @override
+  _Recommend createState() => _Recommend();
+}
+
+class _Recommend extends State<Recommend> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
