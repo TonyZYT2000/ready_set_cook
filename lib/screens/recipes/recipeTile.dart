b15fb327535f2132d9742d44b45db14f746fcebe
wangkanlin <wangkanlin96@gmail.com>
2020-12-04 19:12:02 -0800

    upadated recipeUI

--- lib/screens/recipes/recipeTile.dart
+++ lib/screens/recipes/recipeTile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ready_set_cook/screens/recipes/viewRecipe.dart';
 
+// ignore: must_be_immutable
 class RecipeTile extends StatelessWidget {
   final String name;
   final double rating;
@@ -16,48 +17,87 @@ class RecipeTile extends StatelessWidget {
               MaterialPageRoute(builder: (context) => ViewRecipe(recipeId)));
         },
         child: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: SingleChildScrollView(
-                child: Column(children: <Widget>[
-              Image(
-                image: AssetImage("assets/images/apple.png"),
-                height: 140,
-                alignment: Alignment.centerLeft,
-              ),
-              SizedBox(height: 10),
-              Center(
-                  child: Text(name + '\n' + lst[1] + rating.toString(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontSize: 18,
-                          color: Colors.blueGrey)))
-            ]))));
-    // padding: EdgeInsets.all(10.0),
-    // width: 120,
-    // height: 120,
-    // decoration: BoxDecoration(
-    //   border: Border.all(color: Colors.blue[100], width: 10),
-    //   borderRadius: BorderRadius.circular(20),
-    //   image:
-    //       DecorationImage(image: AssetImage("assets/images/apple.png")),
-    // ),
-    // child: Center(
-    //     child: Text(
-    //         lst[0] +
-    //             name +
-    //             '   ' +
-    //             lst[1] +
-    //             cooked +
-    //             '   ' +
-    //             lst[2] +
-    //             rating.toString(),
-    //         textAlign: TextAlign.left,
-    //         style: TextStyle(fontSize: 18))),
-    // image: DecorationImage(
-    //     colorFilter: ColorFilter.mode(
-    //         Colors.blue.withOpacity(1.0), BlendMode.softLight),
-    //     fit: BoxFit.cover,
-    //     image: AssetImage("assets/images/apple.png"))),
+          padding: EdgeInsets.all(18),
+          margin: EdgeInsets.all(20),
+          child: Wrap(children: [
+            Column(children: <Widget>[
+              new Container(
+                  height: 140,
+                  decoration: new BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/apple.png"),
+                      ),
+                      borderRadius: new BorderRadius.circular(8)))
+            ]),
+
+            SizedBox(height: 10),
+
+            //Center(
+
+            //child:
+
+            Text(name + '\n' + "Rate: " + rating.toString(),
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 18,
+                    color: Colors.blueGrey)),
+
+            _buildRatingStar(rating),
+          ]),
+          width: 150,
+          height: 260,
+          decoration: BoxDecoration(
+              color: Colors.blue[100], borderRadius: BorderRadius.circular(10)),
+        ));
   }
 }
+
+Widget _buildRatingStar(rating) {
+  List<Widget> icons = [];
+  double i = 0;
+  while (i < rating / 2 - 0.5) {
+    icons.add(new Icon(Icons.star, color: Colors.blue));
+
+    i = i + 1;
+  }
+  if (rating / 2 - i >= 0.5) {
+    icons.add(new Icon(Icons.star_half, color: Colors.blue));
+  }
+
+  while (icons.length != 5) {
+    icons.add(new Icon(Icons.star_border, color: Colors.grey));
+  }
+
+  return new Padding(
+    padding: EdgeInsets.fromLTRB(70, 0, 0, 10),
+    child: new Row(children: icons),
+  );
+}
+
+// padding: EdgeInsets.all(10.0),
+// width: 120,
+// height: 120,
+// decoration: BoxDecoration(
+//   border: Border.all(color: Colors.blue[100], width: 10),
+//   borderRadius: BorderRadius.circular(20),
+//   image:
+//       DecorationImage(image: AssetImage("assets/images/apple.png")),
+// ),
+// child: Center(
+//     child: Text(
+//         lst[0] +
+//             name +
+//             '   ' +
+//             lst[1] +
+//             cooked +
+//             '   ' +
+//             lst[2] +
+//             rating.toString(),
+//         textAlign: TextAlign.left,
+//         style: TextStyle(fontSize: 18))),
+// image: DecorationImage(
+//     colorFilter: ColorFilter.mode(
+//         Colors.blue.withOpacity(1.0), BlendMode.softLight),
+//     fit: BoxFit.cover,
+//     image: AssetImage("assets/images/apple.png"))),
