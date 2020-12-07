4582576766a92bac5e918cc3aa831481bbb726b8
rickyli888 <rickyli860@gmail.com>
2020-12-07 14:16:43 -0500

    Better Looking Tiles

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -17,7 +17,7 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> _widgetOptions = <Widget>[
-    Recipe(),
+    Recommend(),
     Recipe(),
     Storage(),
     Profile(),
