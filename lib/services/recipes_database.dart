74b997cc08e206b3a12ea21e355ff8eddc5b5f9e
h4wang489 <h4wang@ucsd.edu>
2020-12-08 01:58:42 -0800

    favorite and rating system

--- lib/services/recipes_database.dart
+++ lib/services/recipes_database.dart
@@ -54,8 +54,8 @@ class RecipesDatabaseService {
       if (ds != null) {
         ds.docs.forEach((recipe) {
           id = recipe['recipeId'];
-          print("The id is");
-          print(id);
+          // print("The id is");
+          // print(id);
           if (id == recipeId) {
             print("we are in");
             return FirebaseFirestore.instance
